83e77404ea3d3ba13b1c816c4e32d130d7d4fa1b
Dafne Magenta Luna <dafne.l@adext.com>
2018-08-11 14:45:33 -0500

    kanban MVP

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+
+    <div class="page-header">
+      <h1>Kanban Board</h1>
+      <p class="lead">An example of a Kanban board, written in Vue.js</p>
+    </div>
+
+    <menu-bar></menu-bar>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MenuBar from '@/components/MenuBar'
+
+export default {
+  name: 'app',
+  components: {
+    'menu-bar': MenuBar
+  }
+}
+</script>
+
+<style>
+  .page-header h1 {
+    font-weight: 300;
+  }
+</style>
